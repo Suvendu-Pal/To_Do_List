bf00a8beb700662d79fa3d549f33dbef80c3f85b
Suvendu Pal <sr1577304@gmail.com>
2026-02-21 02:17:01 +0530

    Add files via upload
    📝 A simple, interactive To-Do List web app featuring a modern glassmorphism UI. Built with Vanilla JavaScript, HTML, and CSS.

---
+++ index.js
@@ -0,0 +1,21 @@
+const task = document.getElementById("task");
+const addTaskBtn = document.getElementById("add-task-btn");
+const taskContainer = document.getElementById("tasks-container");
+
+addTaskBtn.addEventListener("click", () => { 
+    const todoWrapper = document.createElement("div");
+    const deleteButton = document.createElement("button");
+    const taskText = document.createElement("p");
+    todoWrapper.classList.add("todo-container");
+    deleteButton.classList.add("delete-button");
+    deleteButton.textContent = "DELETE";
+    taskText.classList.add("task-text");
+    taskText.textContent = task.value;
+    todoWrapper.append(taskText);
+    todoWrapper.append(deleteButton);
+    taskContainer.append(todoWrapper);
+    deleteButton.addEventListener("click", () =>{
+        todoWrapper.remove();
+    });
+    task.value = "";
+});
